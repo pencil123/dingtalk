e98527ddcd15bbc0eab5bdf93fb27ea2cd176cd2
张鲁燕 <zhangluyan@jsh.com>
2019-11-29 16:29:52 +0800

    init

---
+++ src/main/java/cn/blogs/dingtalk/Prometheus.java
@@ -0,0 +1,48 @@
+package cn.blogs.dingtalk;
+
+import com.alibaba.fastjson.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lyzhang
+ * @since 2019/11/29 15:43
+ */
+@RestController
+@RequestMapping("/")
+public class Prometheus {
+
+  @RequestMapping(value = "/receive", method = RequestMethod.POST)
+  public void alterMsgRev(HttpServletRequest request){
+    JSONObject jsonParam = this.getJSONParam(request);
+  }
+
+  public JSONObject getJSONParam(HttpServletRequest request){
+    JSONObject jsonParam = null;
+    try {
+      // 获取输入流
+      BufferedReader
+          streamReader = new BufferedReader(new InputStreamReader(request.getInputStream(), "UTF-8"));
+
+      // 写入数据到Stringbuilder
+      StringBuilder sb = new StringBuilder();
+      String line = null;
+      while ((line = streamReader.readLine()) != null) {
+        sb.append(line);
+      }
+      jsonParam = JSONObject.parseObject(sb.toString());
+      // 直接将json信息打印出来
+      System.out.println(jsonParam.toJSONString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return jsonParam;
+  }
+}
+
+
+
---
+++ src/test/java/cn/blogs/dingtalk/DingtalkApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.blogs.dingtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingtalkApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
