910dd691ff4158ca35647ade6b8c5556ec03e657
lyzhang <tianxiamall@163.com>
2020-11-04 13:39:20 +0800

    并发编程-synchronized 并发锁设置

--- src/main/java/cn/blogs/dingtalk/utils/MsgSend2.java
+++ src/main/java/cn/blogs/dingtalk/utils/MsgSend2.java
@@ -1,6 +1,7 @@
 package cn.blogs.dingtalk.utils;
 
 import com.alibaba.fastjson.JSONObject;
+import lombok.Singular;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
@@ -13,29 +14,32 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@Scope(scopeName = "singleton")
 public class MsgSend2 {
     @Value("${dingtalk.url}")
     public String WEBHOOK_TOKEN;
-
     private Logger logger = LoggerFactory.getLogger(MsgSend2.class);
 
-    public String send(JSONObject message) {
+    public synchronized  String send(JSONObject message) {
         String returnValue = "这是默认返回值，接口调用失败";
         CloseableHttpClient httpclient = HttpClients.createDefault();
-
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         HttpPost httpPost = new HttpPost(WEBHOOK_TOKEN);
-
         logger.info(message.toJSONString());
         StringEntity requestEntity = new StringEntity(message.toJSONString(),"utf-8");
         requestEntity.setContentEncoding("UTF-8");
         httpPost.setHeader("Content-type", "application/json");
         httpPost.setEntity(requestEntity);
-
         try{
             HttpResponse response  = httpclient.execute(httpPost);
             HttpEntity entity = response.getEntity();
@@ -45,7 +49,7 @@ public class MsgSend2 {
         }catch (Exception e){
             e.printStackTrace();
         }
-        logger.info("return info : {}",returnValue);
+        logger.info("钉钉调用返回信息: {}",returnValue);
         try {
             httpclient.close();
         } catch (IOException e) {
